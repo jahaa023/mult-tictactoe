d52a8bd5d9f9a256f04974fe46c237c97a5467f3
Jakob Johannes Hansgaard <jakob.johannes.hansgaard@outlook.com>
2025-01-01 03:17:10 +0100

    more stuff

--- tictactoemult/views.py
+++ tictactoemult/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        data = request.POST
+        username = data.get("username")
+        password = data.get("password")
+        return HttpResponse(username + password)
+    else:
+        return HttpResponseNotAllowed()
