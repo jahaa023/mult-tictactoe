130795055ed48e0ed61dde9d7c84ec3566e01232
Jakob Johannes Hansgaard <jakob.johannes.hansgaard@outlook.com>
2025-01-09 21:41:06 +0100

    revamped styling of main page

--- tictactoemult/static/js/main.js
+++ tictactoemult/static/js/main.js
@@ -1,1 +1,31 @@
 // JavaScript file for the main page
+
+// jQuery animations for play buttons
+
+$('#header-play-button-1').mouseenter(function() {
+    $('#header-play-button-1').stop()
+    $('#header-play-button-1').animate({
+        width: '500px'
+    });
+})
+
+$('#header-play-button-1').mouseleave(function() {
+    $('#header-play-button-1').stop()
+    $('#header-play-button-1').animate({
+        width: '400px'
+    });
+})
+
+$('#header-play-button-2').mouseenter(function() {
+    $('#header-play-button-2').stop()
+    $('#header-play-button-2').animate({
+        width:'500px'
+    });
+})
+
+$('#header-play-button-2').mouseleave(function() {
+    $('#header-play-button-2').stop()
+    $('#header-play-button-2').animate({
+        width:'400px'
+    });
+})
