4d96f23a533c2d38b4c1602da4cfb4ea85fbc8e7
unknown <jakob.johannes.hansgaard@outlook.com>
2025-01-17 18:44:12 +0100

    registered the new models

--- tictactoemult/admin.py
+++ tictactoemult/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import users, recovery_codes
+from .models import users, recovery_codes, friend_list, pending_friends
 
 # Register your models here.
 admin.site.register(users)
 admin.site.register(recovery_codes)
+admin.site.register(friend_list)
+admin.site.register(pending_friends)
